8b718d4e7f49fdfccff9114b69265d7ff53b8f1f
mdtauseef0639 <31138024+mdtauseef0639@users.noreply.github.com>
2020-02-09 22:05:53 +0530

    Create Binary.py

---
+++ Binary.py
@@ -0,0 +1,18 @@
+a=[]
+n=int(input())
+if n==0:
+    print(0)
+elif n==0:
+    print(1)
+else:
+    while n!=1:
+        rem=n%2
+        n=n//2
+        a.append(rem)
+    
+    a.append(n)
+a=a[::-1]
+
+
+
+res = sum(d * 10**i for i, d in enumerate(a[::-1]))
